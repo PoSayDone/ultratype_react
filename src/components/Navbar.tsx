a31d9fa180d38adfa9d808b120887400392d10be
yosheeeee <evantipin@edu.hse.com>
2023-03-26 13:00:29 +0500

    вынес элемент навигации в отдельную компоненту

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,49 +4,37 @@ interface NavbarProps{
     language : boolean
 }
 
+interface NavItemProps{
+    src: string
+    spanText: string
+    language: boolean
+    innerText: string[] //первый в массиве текст русский, второй английский
+}
+
+
+//отрефракторил немного и вынес ячейки навигации в отдельную компоненту
+const NavItem = ({src,spanText,language,innerText} : NavItemProps) =>{
+    return(
+        <NavLink to={src} className={({ isActive }) => (isActive ? 'navbar__item navbar__item_active' : 'navbar__item')}>
+                <div className="navbar__item_box">
+                    <span className="material-symbols-rounded">
+                        {spanText}
+                    </span>
+                </div>
+                <div className="navbar__item_text">
+                    {language? innerText[0] : innerText[1]}
+                </div>
+            </NavLink>
+    )
+}
+
 const Navbar = ({language} : NavbarProps) => {
     return (
         <nav>
-            <NavLink to="/" className={({ isActive }) => (isActive ? 'navbar__item navbar__item_active' : 'navbar__item')}>
-                <div className="navbar__item_box">
-                    <span className="material-symbols-rounded">
-                        home
-                    </span>
-                </div>
-                <div className="navbar__item_text">
-                    {language? 'Главная' : 'Home'}
-                </div>
-            </NavLink>
-            <NavLink to="/typing" className={({ isActive }) => (isActive ? 'navbar__item navbar__item_active' : 'navbar__item')}>
-                <div className="navbar__item_box">
-                    <span className="material-symbols-rounded">
-                        notes
-                    </span>
-                </div>
-                <div className="navbar__item_text">
-                    {language? 'Печать': 'Typing'}
-                </div>
-            </NavLink>
-            <NavLink to="/settings" className={({ isActive }) => (isActive ? 'navbar__item navbar__item_active' : 'navbar__item')}>
-                <div className="navbar__item_box">
-                    <span className="material-symbols-rounded">
-                        settings
-                    </span>
-                </div>
-                <div className="navbar__item_text">
-                    {language? 'Настройки': 'Settings'}
-                </div>
-            </NavLink>
-            <NavLink to="/profile" className={({ isActive }) => (isActive ? 'navbar__item navbar__item_active' : 'navbar__item')}>
-                <div className="navbar__item_box">
-                    <span className="material-symbols-rounded">
-                        person
-                    </span>
-                </div>
-                <div className="navbar__item_text">
-                    {language? 'Профиль': 'Profile'}
-                </div>
-            </NavLink>
+            <NavItem src="/" spanText="home" language={language} innerText={['Главная','Home']}/>
+            <NavItem src="/typing" spanText="notes" language={language} innerText={['Печать','Typing']}/>
+            <NavItem src="/settings" spanText="settings" language={language} innerText={['Настройки','Settings']}/>
+            <NavItem src="/profile" spanText="person" language={language} innerText={['Профиль','Profile']}/>
         </nav>
     )
 }
