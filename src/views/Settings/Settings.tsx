395928906680272bfaa362b8e0e9110dae050dbf
posaydone <poseaydone@ya.ru>
2023-03-26 14:17:28 +0500

    Зануление стилей в отдельный файл, перемещены стили для общих элементов в App.scss, небольшое изменение сруктруры Settings.tsx

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -27,47 +27,51 @@ const Settings = ({ theme, setTheme, language, setLanguage, font, setFont }: Set
 
     return (
         <div className="settings">
-            <h1>{language ? 'Настройки' : 'Settings'}</h1>
-            <h2>{language ? 'Интерфейс' : 'Interface'}</h2>
-            <form name='setting'>
-                <fieldset name='language'>
-                    <p>{language ? 'Язык' : 'Language'}</p>
-                    <select onChange={changeLanguage}>
-                        {
-                            language ?
-                            <option value="ru" selected>Русский</option>
-                            : <option value="ru" >Русский</option>
-                        
-                        }
-                        {
-                            language?
-                            <option value="eng">English</option>
-                            : <option value="eng" selected>English</option>
-                        }
-                        
-                    </select>
-                </fieldset>
-                <fieldset name='theme'>
-                    <p>{language ? 'Тема' : 'Theme'}</p>
-                    <select onChange={changeTheme}>
-                        {theme ?
-                            <option value="dark" selected>{language ? 'Темная' : 'Dark'}</option>
-                            : <option value="dark" >{language ? 'Темная' : 'Dark'}</option>
-                        }
+            <div className="title__section">
+                <h1>{language ? 'Настройки' : 'Settings'}</h1>
+            </div>
+            <div className="interface__section">
+                <h2>{language ? 'Интерфейс' : 'Interface'}</h2>
+                <form name='setting'>
+                    <fieldset name='language'>
+                        <p>{language ? 'Язык' : 'Language'}</p>
+                        <select onChange={changeLanguage}>
+                            {
+                                language ?
+                                <option value="ru" selected>Русский</option>
+                                : <option value="ru" >Русский</option>
+                            
+                            }
+                            {
+                                language?
+                                <option value="eng">English</option>
+                                : <option value="eng" selected>English</option>
+                            }
+                            
+                        </select>
+                    </fieldset>
+                    <fieldset name='theme'>
+                        <p>{language ? 'Тема' : 'Theme'}</p>
+                        <select onChange={changeTheme}>
+                            {theme ?
+                                <option value="dark" selected>{language ? 'Темная' : 'Dark'}</option>
+                                : <option value="dark" >{language ? 'Темная' : 'Dark'}</option>
+                            }
 
-                        {theme ?
-                            <option value="light">{language ? 'Светлая' : 'Light'}</option>
-                            : <option value="light" selected>{language ? 'Светлая' : 'Light'}</option>
-                        }
+                            {theme ?
+                                <option value="light">{language ? 'Светлая' : 'Light'}</option>
+                                : <option value="light" selected>{language ? 'Светлая' : 'Light'}</option>
+                            }
 
 
-                    </select>
-                </fieldset>
-                <fieldset name='font'>
-                    <p>{language ? 'Моноширный шрифт' : 'Monospase font'}</p>
-                    <div className={font ? 'slider right' : 'slider left'} onClick={() => setFont((prev: any) => !prev)}></div>
-                </fieldset>
-            </form>
+                        </select>
+                    </fieldset>
+                    <fieldset name='font'>
+                        <p>{language ? 'Моноширный шрифт' : 'Monospase font'}</p>
+                        <div className={font ? 'slider right' : 'slider left'} onClick={() => setFont((prev: any) => !prev)}></div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 }
