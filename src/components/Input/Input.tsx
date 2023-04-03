2004a3006366375e0affaa1633adab0a308f1845
yosheeeee <evantipin@edu.hse.com>
2023-04-03 11:17:33 +0500

    пофиксил сохранение состояния выбранной темы, языка и шрифта в настройках

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { createContext, forwardRef, useCallback, useContext, useEffect, useRef, useState } from "react";
+import {createContext, forwardRef, useCallback, useContext, useEffect, useRef, useState} from "react";
 import Heading from "../Heading/Heading"
 import classNames from "classnames";
 import Character from "../Character";
@@ -6,73 +6,73 @@ import Caret from "../Caret";
 
 // Пропсы для инпута
 type InputProps = {
-    // Текст вводимый пользователем
-    userText: string;
-    // Текст задания
-    text: string;
-    cursorPosition: number;
-    currentCharacterRef: React.RefObject<HTMLSpanElement>
-    state : string
+	// Текст вводимый пользователем
+	userText: string;
+	// Текст задания
+	text: string;
+	cursorPosition: number;
+	currentCharacterRef: React.RefObject<HTMLSpanElement>
+	state: string
 }
 
 // Компонент input
-const Input = ({ userText, text, cursorPosition , currentCharacterRef , state}: InputProps) => {
-    const userTextArray: string[] = userText.split(/(?<=\s)/)
-    const textArray = text.split(/(?<=\s)/)
+const Input = ({userText, text, cursorPosition, currentCharacterRef, state}: InputProps) => {
+	const userTextArray: string[] = userText.split(/(?<=\s)/)
+	const textArray = text.split(/(?<=\s)/)
 
-    const [leftMargin, setLeftMargin] = useState<any | null>(null);
-    const [topMargin, setTopMargin] = useState<any | null>(null);
-    let previousWordsLength = 0 // Переменная для преобразование индексов двумерного массива в одномерный
+	const [leftMargin, setLeftMargin] = useState<any | null>(null);
+	const [topMargin, setTopMargin] = useState<any | null>(null);
+	let previousWordsLength = 0 // Переменная для преобразование индексов двумерного массива в одномерный
 
-    const calculateLeftMargin = useEffect(
-        () => {
-            currentCharacterRef.current !== undefined ? setLeftMargin(currentCharacterRef.current?.offsetLeft) : 0
-            currentCharacterRef.current !== undefined ? setTopMargin(currentCharacterRef.current?.offsetTop) : 0
-        },
-        [cursorPosition],
-    )
+	const calculateLeftMargin = useEffect(
+		() => {
+			currentCharacterRef.current !== undefined ? setLeftMargin(currentCharacterRef.current?.offsetLeft) : 0
+			currentCharacterRef.current !== undefined ? setTopMargin(currentCharacterRef.current?.offsetTop) : 0
+		},
+		[cursorPosition]
+	)
 
-    // Возвращаем div'ы слов, состоящие из Character'ов
-    return (
-        <>
-            {
-               state != 'finish' ? <Caret leftMargin={leftMargin} topMargin={topMargin}/> : ''
-            }
-            {
-                textArray.map((word, wordIndex) => { // Проходимся по всем словам при помощи map
-                    {
-                        // Получаем длины предыдущих слов для того, чтобы знать положение указателя в тексте
-                        previousWordsLength += wordIndex === 0 ? 0 : textArray[wordIndex - 1].length
-                    }
-                    return (
-                        <div className="input__word">{ // Возвращаем div
-                            word.split("").map((character, characterIndex) => { // Проходимся по всем символам в слове при помощи map
-                                {
-                                    useEffect(() => {
-                                    }, [cursorPosition, currentCharacterRef])
-                                }
-                                const isActive = (previousWordsLength + characterIndex === cursorPosition)
-                                return (
-                                    <>
-                                        <Character // Возвращаем символ
-                                            ref={isActive ? currentCharacterRef : null}
-                                            expected={character} // Эталонное значение
-                                            actual={ // Значение пользователя
-                                                userTextArray[wordIndex] !== undefined // Если пользователь ввел символ
-                                                    ? userTextArray[wordIndex].split("")[characterIndex] // То записываем значение этого символа
-                                                    : undefined // Иначе пустое значение
-                                            }
-                                        />
-                                    </>
-                                )
-                            })
-                        }
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
+	// Возвращаем div'ы слов, состоящие из Character'ов
+	return (
+		<>
+			{
+				state != 'finish' ? <Caret leftMargin={leftMargin} topMargin={topMargin}/> : ''
+			}
+			{
+				textArray.map((word, wordIndex) => { // Проходимся по всем словам при помощи map
+					{
+						// Получаем длины предыдущих слов для того, чтобы знать положение указателя в тексте
+						previousWordsLength += wordIndex === 0 ? 0 : textArray[wordIndex - 1].length
+					}
+					return (
+						<div className="input__word">{ // Возвращаем div
+							word.split("").map((character, characterIndex) => { // Проходимся по всем символам в слове при помощи map
+								{
+									useEffect(() => {
+									}, [cursorPosition, currentCharacterRef])
+								}
+								const isActive = (previousWordsLength + characterIndex === cursorPosition)
+								return (
+									<>
+										<Character // Возвращаем символ
+											ref={isActive ? currentCharacterRef : null}
+											expected={character} // Эталонное значение
+											actual={ // Значение пользователя
+												userTextArray[wordIndex] !== undefined // Если пользователь ввел символ
+													? userTextArray[wordIndex].split("")[characterIndex] // То записываем значение этого символа
+													: undefined // Иначе пустое значение
+											}
+										/>
+									</>
+								)
+							})
+						}
+						</div>
+					)
+				})
+			}
+		</>
+	)
 }
 
 export default Input;
--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -1,66 +1,76 @@
 import * as React from 'react';
-import { useContext } from 'react';
-import { ThemeContext } from '../../contexts/ThemeContext';
+import {useContext} from 'react';
+import {ThemeContext} from '../../contexts/ThemeContext';
 import Heading from '../../components/Heading/Heading';
 
 interface SettingsProps {
-    language: boolean
-    setLanguage: Function
-    font: boolean
-    setFont: Function
+	language: boolean
+	setLanguage: Function
+	font: boolean
+	setFont: Function
 }
 
-const Settings = ({ language, setLanguage, font, setFont }: SettingsProps) => {
-    const { theme, setTheme } = useContext(ThemeContext);  
+const Settings = ({language, setLanguage, font, setFont}: SettingsProps) => {
+	const {theme, setTheme} = useContext(ThemeContext);
 
-    const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setTheme(event.target.value);
-    };
+	const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setTheme(event.target.value);
+	};
 
-    function changeLanguage(event: React.ChangeEvent<HTMLSelectElement>) {
-        setLanguage(event.target.value == 'ru' ? true : false)
-    }
+	function changeLanguage(event: React.ChangeEvent<HTMLSelectElement>) {
+		setLanguage(event.target.value == 'ru' ? true : false)
+	}
 
-    return (
-        <div className="settings">
-            <div className="title__section">
-                <Heading headingLevel="h1">{language ? 'Настройки' : 'Settings'}</Heading>
-            </div>
-            <div className="interface__section">
-                <Heading headingLevel="h2">{language ? 'Интерфейс' : 'Interface'}</Heading>
-                <form name='setting'>
-                    <fieldset name='language'>
-                        <p>{language ? 'Язык' : 'Language'}</p>
-                        <select onChange={changeLanguage}>
-                            {
-                                language ?
-                                    <option value="ru" selected>Русский</option>
-                                    : <option value="ru" >Русский</option>
+	return (
+		<div className="settings">
+			<div className="title__section">
+				<Heading headingLevel="h1">{language ? 'Настройки' : 'Settings'}</Heading>
+			</div>
+			<div className="interface__section">
+				<Heading headingLevel="h2">{language ? 'Интерфейс' : 'Interface'}</Heading>
+				<form name='setting'>
+					<fieldset name='language'>
+						<p>{language ? 'Язык' : 'Language'}</p>
+						<select onChange={changeLanguage}>
+							{
+								language
+                                    ? <option value="ru" selected>Русский</option>
+									: <option value="ru">Русский</option>
 
-                            }
-                            {
-                                language ?
-                                    <option value="eng">English</option>
-                                    : <option value="eng" selected>English</option>
-                            }
+							}
+							{
+								language
+                                    ? <option value="eng">English</option>
+									: <option value="eng" selected>English</option>
+							}
 
-                        </select>
-                    </fieldset>
-                    <fieldset name='theme'>
-                        <p>{language ? 'Тема' : 'Theme'}</p>
-                        <select onChange={handleThemeChange}>
-                            <option value="dark" selected>{language ? 'Темная' : 'Dark'}</option>
-                            <option value="light">{language ? 'Светлая' : 'Light'}</option>
-                        </select>
-                    </fieldset>
-                    <fieldset name='font'>
-                        <p>{language ? 'Моноширный шрифт' : 'Monospase font'}</p>
-                        <div className={font ? 'slider right' : 'slider left'} onClick={() => setFont((prev: any) => !prev)}></div>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-    );
+						</select>
+					</fieldset>
+					<fieldset name='theme'>
+						<p>{language ? 'Тема' : 'Theme'}</p>
+						<select onChange={handleThemeChange}>
+							{
+								theme === 'dark'
+									? <option value="dark" selected>{language ? 'Темная' : 'Dark'}</option>
+									: <option value="dark">{language ? 'Темная' : 'Dark'}</option>
+							}
+							{
+								theme === 'light'
+									? <option value="light" selected>{language ? 'Светлая' : 'Light'}</option>
+									:  <option value="light">{language ? 'Светлая' : 'Light'}</option>
+							}
+
+						</select>
+					</fieldset>
+					<fieldset name='font'>
+						<p>{language ? 'Моноширный шрифт' : 'Monospase font'}</p>
+						<div className={font ? 'slider right' : 'slider left'}
+						     onClick={() => setFont((prev: any) => !prev)}></div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Settings;
