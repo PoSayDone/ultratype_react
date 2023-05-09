d6d5d4fe66d506c4a2989dc0822fcd529cd8898a
yosheeeee <evantipin@edu.hse.ru>
2023-05-09 09:46:59 +0300

    добавил логаут в хедер

---
+++ backend/Controllers/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("levels")]
+public class LevelController : ControllerBase
+{
+        private Dictionary<int, string> levelBase = new Dictionary<int, string>()
+        {
+                {1, "fj"},
+                {2,"gh"},
+                {3,"fjgh"},
+                {4,"dk"},
+                {5,"fjghdk"},
+                {6,"sl"},
+                {7,"ghfjdksl"},
+                {8,"a;'"},
+                {9,"asdfghjkl;'"}
+        };
+}
