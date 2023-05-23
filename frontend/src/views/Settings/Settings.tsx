e1c842883a2d95a3ea313e4a6e25c2cfe5a1b04c
PoSayDone <poseaydone@ya.ru>
2023-05-23 22:41:01 +0500

    Фикшу стили настроек

--- frontend/src/views/Settings/Settings.tsx
+++ frontend/src/views/Settings/Settings.tsx
@@ -1,19 +1,19 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Heading from '../../components/Heading/Heading';
-import {TFunction} from 'i18next';
-import {useTypedSelector} from "../../hooks/useTypedSelector";
-import {useDispatch} from "react-redux";
+import { TFunction } from 'i18next';
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
 import AnimatedContainer from '../../components/AnimatedContainer';
-import {Dispatch} from 'redux';
-import {SettingsActions, SettingsActionsTypes} from '../../store/reducers/settingsReducer';
+import { Dispatch } from 'redux';
+import { SettingsActions, SettingsActionsTypes } from '../../store/reducers/settingsReducer';
 
 interface SettingsProps {
     text: TFunction<"translation", undefined, "translation">
 }
 
 const Settings = ({ text }: SettingsProps) => {
-    const {language,isMonospace,theme , timeAttackTime, typingLanguage} = useTypedSelector(state => state.settings)
-    const dispatch : Dispatch<SettingsActions> = useDispatch()
+    const { language, isMonospace, theme, timeAttackTime, typingLanguage } = useTypedSelector(state => state.settings)
+    const dispatch: Dispatch<SettingsActions> = useDispatch()
     const [timeAttackValue, setTimeAttackValue] = useState(`${timeAttackTime}`)
     const changeTheme = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const theme_value = event.target.value //проверка языка
@@ -23,39 +23,37 @@ const Settings = ({ text }: SettingsProps) => {
     function changeLanguage(event: React.ChangeEvent<HTMLSelectElement>) {
         const lang = event.target.value === 'ru' //проверка языка
         dispatch({ type: SettingsActionsTypes.CHANGE_LANGUAGE, payload: lang })
-        localStorage.setItem("language",event.target.value)
+        localStorage.setItem("language", event.target.value)
     }
 
-    const ChangeTimeAttackTimeValue = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        if (event.target.value == ""){
+    const ChangeTimeAttackTimeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value == "") {
             setTimeAttackValue("0")
-        }else{
-            let value = event.target.value[0] === "0" && event.target.value.length!=1 ? event.target.value.slice(1) : event.target.value
+        } else {
+            let value = event.target.value[0] === "0" && event.target.value.length != 1 ? event.target.value.slice(1) : event.target.value
             setTimeAttackValue(value)
         }
     }
 
     const ChangeTimeAttackTime = (event: React.FocusEvent<HTMLInputElement>) => {
-        dispatch({type: SettingsActionsTypes.SET_TIMEATTACK_TIME, payload: +event.target.value <1 ? 1 : +event.target.value})
-        localStorage.setItem("timerAttackTime", `${+event.target.value <1 ? 1 : +event.target.value}`)
+        dispatch({ type: SettingsActionsTypes.SET_TIMEATTACK_TIME, payload: +event.target.value < 1 ? 1 : +event.target.value })
+        localStorage.setItem("timerAttackTime", `${+event.target.value < 1 ? 1 : +event.target.value}`)
     }
 
-    function changeTypingLanguage(event: React.ChangeEvent<HTMLSelectElement>){
-        dispatch({type:SettingsActionsTypes.SET_TYPING_LANGUAGE,payload:event.target.value})
-        localStorage.setItem("typingLang",event.target.value)
+    function changeTypingLanguage(event: React.ChangeEvent<HTMLSelectElement>) {
+        dispatch({ type: SettingsActionsTypes.SET_TYPING_LANGUAGE, payload: event.target.value })
+        localStorage.setItem("typingLang", event.target.value)
     }
 
     return (
         <AnimatedContainer>
             <div className="settings">
-                <div className="title__section">
-                    <Heading headingLevel="h1">{text("settings.title")}</Heading>
-                </div>
-                <div className="interface__section">
-                    <Heading headingLevel="h2">{text("settings.interface")}</Heading>
-                    <form name='settings'>
+                <Heading headingLevel="h1">{text("settings.title")}</Heading>
+                <form name='settings'>
+                    <div className="settings__section">
+                        <Heading headingLevel="h2">{text("settings.interface")}</Heading>
                         <fieldset name='language'>
-                            <p>{text("settings.language")}</p>
+                            <span>{text("settings.language")}</span>
                             <select onChange={changeLanguage}>
                                 {
                                     language
@@ -72,7 +70,7 @@ const Settings = ({ text }: SettingsProps) => {
                             </select>
                         </fieldset>
                         <fieldset name='theme'>
-                            <p>{text("settings.theme")}</p>
+                            <span>{text("settings.theme")}</span>
                             <select onChange={changeTheme}>
                                 {
                                     theme === 'dark'
@@ -88,17 +86,19 @@ const Settings = ({ text }: SettingsProps) => {
                             </select>
                         </fieldset>
                         <fieldset name='font'>
-                            <p>{text("settings.font")}</p>
+                            <span>{text("settings.font")}</span>
                             <div className={isMonospace ? 'slider right' : 'slider left'}
                                 onClick={() => {
                                     dispatch({ type: SettingsActionsTypes.CHANGE_FONT })
-                                    localStorage.setItem("isMonospace",`${isMonospace}`)
+                                    localStorage.setItem("isMonospace", `${isMonospace}`)
                                     console.log(`${isMonospace}`);
-                                    }}></div>
+                                }}></div>
                         </fieldset>
+                    </div>
+                    <div className='settings__section'>
                         <Heading headingLevel={"h2"}>Язык печати</Heading>
                         <fieldset name='typingLanguage'>
-                            <p>{text("settings.language")}</p>
+                            <span>{text("settings.language")}</span>
                             <select onChange={changeTypingLanguage}>
                                 {
                                     typingLanguage == "ru"
@@ -114,16 +114,17 @@ const Settings = ({ text }: SettingsProps) => {
 
                             </select>
                         </fieldset>
+                    </div>
+                    <div className='settings__section'>
                         <Heading headingLevel={"h2"}>{text("settings.timeAttack")}</Heading>
                         <fieldset name='timeAttack'>
-                            <p>{text("settings.timeAttackTime")}</p>
-                            <input type="number" name="timeAttacktime" value={timeAttackValue} onChange={ChangeTimeAttackTimeValue} onBlur={ChangeTimeAttackTime}/>
+                            <span>{text("settings.timeAttackTime")}</span>
+                            <input type="number" name="timeAttacktime" value={timeAttackValue} onChange={ChangeTimeAttackTimeValue} onBlur={ChangeTimeAttackTime} />
                         </fieldset>
-
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
-        </AnimatedContainer>
+        </AnimatedContainer >
     );
 }
 
